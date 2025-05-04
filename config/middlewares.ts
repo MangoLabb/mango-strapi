f7cab73418132c63555016955b502ef20dd2b01e
Vikkingg13 <vikkingg13@gmail.com>
2025-05-04 11:40:10 +0300

    Edit middlewares for supabase support

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,29 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
-  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'res.cloudinary.com', // cloudinary images
+            'lh3.googleusercontent.com', // google avatars
+            'platform-lookaside.fbsbx.com', // facebook avatars
+            'dl.airtable.com', // strapi marketplace,
+            "market-assets.strapi.io",
+            env('SUPABASE_API_URL'),
+          ],
+          'media-src': ["'self'", 'data:', 'blob:', env('SUPABASE_API_URL')],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
