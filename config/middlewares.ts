0adbecd9439f3f5f1b153cd9b8ee4bd37a95a64d
Vikkingg13 <vikkingg13@gmail.com>
2025-05-17 15:42:00 +0300

    Revert "Remove custom strapi security"
    This reverts commit fdf38bf516781153e37ce540f6fb9ed83951d57f.

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,5 +1,29 @@
 export default ({ env }) =>  [
   'strapi::logger',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'res.cloudinary.com', // cloudinary images
+            'lh3.googleusercontent.com', // google avatars
+            'platform-lookaside.fbsbx.com', // facebook avatars
+            'dl.airtable.com', // strapi marketplace,
+            "market-assets.strapi.io",
+            env('SUPABASE_API_URL'),
+          ],
+          'media-src': ["'self'", 'data:', 'blob:', env('SUPABASE_API_URL')],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
